869113f313a42363b1fb0a4673d06893caf9cf31
AxWax <axel@axwax.eu>
2022-11-21 22:11:39 +0100

    add example credentials file

---
+++ credentials_example.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+access_token = '' # your Mastodon access token - get it at https://{your mastodon instance}/settings/applications
+api_base_url = 'https://{your mastodon instance}' # your mastodon instance's URL (including https://)
+account_id = 1234 # if choosing to display an account's timeline, enter its account_id here - you can use search_for_account.py to find it
