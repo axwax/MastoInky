b069697df7b0f68652d2619200c23e475ace3b85
AxWax <axel@axwax.eu>
2022-11-21 22:12:13 +0100

    add search for account utility file

---
+++ search_for_account_id.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# search for Mastodon account and return account IDs for each result
+import signal
+from urllib.request import urlopen
+import textwrap
+from mastodon import Mastodon
+from credentials import access_token, api_base_url, account_id
+
+# account to search
+account_to_search = '@axwax@fosstodon.org'
+
+
+# Initialise Mastodon
+mastodon = Mastodon(
+    access_token = access_token,
+    api_base_url = api_base_url
+)
+
+# search for the account and print out the list of results
+results_list = mastodon.account_search(account_to_search, limit=None, following=False)
+for user in results_list:
+    print(user['acct'] + ':', user['id'])
